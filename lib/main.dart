35748c7fd434144fe7bcb902ce7d3a2d1132e4f4
Torresypther <torresypther@gmail.com>
2024-10-08 18:33:12 +0800

    10-10

--- lib/main.dart
+++ lib/main.dart
@@ -9,92 +9,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       body: Center(
-        child: SizedBox(
-          height: 500,
-          width: 500,
+        child: Container(
+          height: 1000, 
+          width: 1000, 
+          color: Colors.amber,
           child: Column(
             children: [
-              Row(
-                children: [
-                  Container(height: 100, width: 100, color: Colors.amberAccent,),
-                  Container(height: 100, width: 150, color: Colors.blue,),
-                  Container(height: 100, width: 50, color: Colors.orange,),
-                  Column(
-                    children: [
-                      Container(height: 50, width: 100, color: Colors.blue,),
-                      Container(height: 50, width: 100, color: Colors.red,),
-                    ],
-                  ),
-                  Container(height: 100, width: 100, color: Colors.black54)
-                ],
-              ), 
-              Row(
-                children: [
-                  Container(height: 200, width: 70, color: Colors.black54),
-                  Column(
-                    children: [
-                      Container(height: 100, width: 70, color: Colors.pink),
-                      Container(height: 50, width: 70, color: Colors.indigo),
-                      Container(height: 50, width: 70, color: Colors.yellow),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Row(
+              SizedBox(
+                height: 300,
+                child: Row(
+                  children: [
+                    Expanded(flex: 3, child: Container(color: Colors.red)),
+                    Expanded(flex: 3, child: Container(color: Colors.green)),
+                    Expanded(flex: 2, child: Container(color: Colors.blue)),
+                    Expanded(
+                      flex: 2,
+                      child: Column(
                         children: [
-                          Container(height: 80, width: 75, color: Colors.purpleAccent),
-                          Container(height: 80, width: 75, color: Colors.redAccent),
+                          Expanded(child: Container(color: Colors.blueGrey)),
+                          Expanded(child: Container(color: Colors.indigo)),
+                          Expanded(
+                            flex: 1, 
+                            child: Row(
+                              children: [
+                                Expanded(child: Container(color: Colors.green)),
+                                Expanded(child: Container(color: Colors.orange))
+                              ],
+                            )
+                          )
                         ],
-                      ),
-                      Container(height: 40, width: 150, color: Colors.tealAccent),
-                      Row(
-                        children: [
-                          Container(height: 60, width: 50, color: Colors.black87),
-                          Container(height: 60, width: 50, color: Colors.deepOrangeAccent),
-                          Container(height: 60, width: 50, color: Colors.deepPurple),
-                        ],
-                      ),
-                      Container(height: 20, width: 150, color: Colors.lightBlue),
-                    ],
-                  ),
-                  Container(height: 200, width: 70, color: Colors.blueGrey),
-                  Column(
-                    children: [
-                      Container(height: 30, width: 40, color: Colors.brown),
-                      Container(height: 70, width: 40, color: Colors.lightBlueAccent), 
-                      Container(height: 100, width: 40, color: Colors.red), 
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(height: 100, width: 100, color: Colors.deepPurple),
-                      Container(height: 100, width: 100, color: Colors.green),
-                    ],
-                  )
-                ],
+                      )
+                    ),
+                  ],
+                ),
               ),
-              Row(
-                children: [
-                  Column(
-                    children: [
-                      Container(height: 75, width: 300, color: Colors.redAccent),
-                      Container(height: 75, width: 300, color: Colors.blue),
-                    ],
-                  ), 
-                  Container(height: 150, width: 100, color: Colors.green), 
-                  Column(
-                    children: [
-                      Container(height: 100, width: 100, color: Colors.yellow),
-                      Container(height: 50, width: 100, color: Colors.orange)
-                    ],
-                  )
-                ],
-                ) 
             ],
-          )
-        )
+          ),
+        ),
       ),
     );
   }
